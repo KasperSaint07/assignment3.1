8b027624bdab1f37da645c1e9c7b8e154836086f
KasperSaint07 <123306320+KasperSaint07@users.noreply.github.com>
2025-04-28 19:29:36 +0500

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        MyHashTable<MyTestingClass, Integer> table = new MyHashTable<>();
+        Random random = new Random();
 
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(10000);
+            String name = "Test" + random.nextInt(10000);
+            table.put(new MyTestingClass(id, name), i);
+        }
+
+        for (int i = 0; i < 11; i++) {
+            int count = 0;
+            var node = table.chainArray[i];
+            while (node != null) {
+                count++;
+                node = node.next;
+            }
+            System.out.println("Bucket " + i + " has " + count + " elements");
+        }
     }
 }
